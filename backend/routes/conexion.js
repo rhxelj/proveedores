55b2cae4bd5ef90bdecfc0c0a02510295fddac03
Rogelio Martin Corsino <rcorsino@gmail.com>
2018-06-13 11:22:14 -0300

    actualizacion

--- backend/routes/conexion.js
+++ backend/routes/conexion.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var conexion = mysql.createConnection({
     user: 'root',
-    password: 'drasan',
+    password: '123456',
     host: 'localhost',
     database: 'BasesGenerales'
 });
