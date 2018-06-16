b3a510e8e0b9fd6a74d0a046e6a8524fc13e054c
sandrapk <sperezk@yahoo.com.ar>
2018-06-16 17:58:58 -0300

    antes lo subi erroneamente, ahora si

--- backend/routes/actualizamonedas.js
+++ backend/routes/actualizamonedas.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var conexion = require('./conexion');
-var bodyParser = require('body-parser');
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded());
 
 conexion.connect(function(err) {
     if (!err) {
@@ -18,63 +15,31 @@ conexion.connect(function(err) {
 
 
 var router = express();
-// var formulario = '<form method="post" action="/actualizamonedas">' +
-//     '<label for="codigo">Código  </label>' +
-//     '<input type="text" name="idtipomonedas" id="idtipomonedas"/><BR>' +
-//     '<label for="descripcion">Descripción  </label>' +
-//     '<input type="text" name="tipomonedadesc" id="tipomonedadesc"/><BR>' +
-//     '<input type="submit" value="Enviar"/>' +
-//     '</form>';
 
-//     var cabecera = '<h1> Muesta Datos de artículos </h1>';
-// router.get('/?:id', function(req, res, next) {
-//     indice = req.params.id;
-// //    conexion.query('Select * from TipoMonedas order by idTipoMonedas ' ,
-//    conexion.query('Select idTipoMonedas, TipoMonedasDescripcion from TipoMonedas where idTipoMonedas = "' + indice + '"' ,
-//         function(err, result) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//        /*          tipomonedadesc = JSON.stringify(result.rows);
-//                 var tipomon = JSON.parse(tipomonedadesc);
-//                 idtipomonedas = tipomon[0].idtipomonedas;
-//                 tipomonedadesc = tipomon[0].tipomonedadesc;
-//                 res.send('<html><body>' +
-//                 cabecera +
-//                     formulario +
-//                     '</html></body>'); */
-//                  res.json(result);
-                
 
-//             }
-//         });
-//     });
+
+var router = express();
 console.log('estamos aca');
 
-router.post('/?id', function(req, res, next) {
- 
-  var  indice = req.params.id;
-    console.log(indice);
-  //  var registro = {
-      // var TipoMonDes = req.body.tipomonedadesc;
-        //,precio: req.body.precio}
-        var TipoMonDes = tipomonedadesc;
- //   conexion.query('Select * from TipoMonedas where idTipoMonedas = "' + indice ,
-  //UPDATE `BasesGenerales`.`TipoMonedas` SET `TipoMonedasDescripcion`='ujuju' WHERE `idTipoMonedas`='ddd';
+router.post('/?:id', function(req, res, next) {
+ indice = req.params.id;
 
-   conexion.query('UPDATE TipoMonedas SET TipoMonedasDescripcion = ' + "lindos" + ' WHERE idTipoMonedas= "' + indice + '"', 
-         function(err, result) {
+ descr = req.body.TipoMonedasDescripcion;
+ cotiz = req.body.TipoMonedasCotizacion;
+
+
+   // conexion.query('Select * from TipoMonedas where idTipoMonedas = "' + indice + '"',
+    conexion.query('UPDATE TipoMonedas SET TipoMonedasDescripcion = "' + descr + '", TipoMonedasCotizacion = '+ cotiz + ' WHERE idTipoMonedas = "' + indice + '"',
+        function(err, result) {
             if (err) {
                 console.log(err);
             } else {
                 res.json(result);
-       
                 };
-              
-    
-
             });
         });
+
+
 
 
 module.exports = router;
