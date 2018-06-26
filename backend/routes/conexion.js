f1e14381452286dd3d0d9234911f73297503dcd1
Rogelio Martin Corsino <rcorsino@gmail.com>
2018-06-26 08:31:05 -0300

    merge con lo de sandra

--- backend/routes/conexion.js
+++ backend/routes/conexion.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var conexion = mysql.createConnection({
     user: 'root',
-    password: '123456',
+    password: 'password',
     host: 'localhost',
     database: 'BasesGenerales'
 });
