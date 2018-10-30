72a75fa1eaa56d206efb923452f474b9aba3201b
sandrapk <sperezk@yahoo.com.ar>
2018-10-30 08:27:33 -0300

    No fue cambiado pero para que lo actualices

--- backend/routes/stkmonedasmodificar.js
+++ backend/routes/stkmonedasmodificar.js
@@ -28,6 +28,7 @@ indice = req.params.id;
  descr = req.body.StkMonedasDescripcion;
  cotiz = req.body.StkMonedasCotizacion;
 
+ console.log(cotiz)
    conexion.query ('UPDATE StkMonedas SET StkMonedasDescripcion = "' + descr + '", StkMonedasCotizacion = ' + cotiz + ' WHERE idStkMonedas = "' + indice + '"',
 
         function(err, result) {
